dfb80fc52a64cc8376a8a6570732ed4273a6d023
hayden <z5290702@unsw.edu.au>
2022-03-25 20:47:07 +1100

    fixed the cmake

---
+++ nmp_test.cpp
@@ -0,0 +1,9 @@
+#include "gtest/gtest.h"
+#include "main.cpp"
+
+// Demonstrate some basic assertions.
+TEST(nmp_test, BasicAssertions) {
+  // Expect two strings not to be equal.
+  ASSERT_EQ (5000, testerFunction());
+}
+
